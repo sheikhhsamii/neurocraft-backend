afb8d282f738462d5dfa24c12378b3738ea04ecf
sheikhhsamii <sheikhhabdulsami@gmail.com>
2026-05-23 22:10:02 +0500

    notes app

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const frontendUrl =
+    configService.get<string>('FRONTEND_URL') ?? 'http://localhost:5173';
+
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+  });
+
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }),
-);
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
